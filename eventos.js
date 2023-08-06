a4d0f8f79250d577e05172fa9ee884eac85910e1
matonp93 <matiasnp12@gmail.com>
2023-08-06 19:07:04 -0300

    modifiqué eventos y agregue un alert hola soy el div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    let div= document.getElementById("divs")
+    console.log(div)
+
+div.addEventListener('click',()=>{alert("Hola! Soy el div")})
+})
