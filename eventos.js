da0c1d35d75614df00db1287c3c67265ebba8fe0
matonp93 <matiasnp12@gmail.com>
2023-08-08 20:13:15 -0300

    Solución al problema disparo del click en div

--- eventos.js
+++ eventos.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     let div= document.getElementById("divs")
     console.log(div)
-
-div.addEventListener('click',()=>{alert("Hola! Soy el div")})
+//Corrección del problema al presionar el botón y que se ejecute acción del div//
+    const boton = document.getElementById("miBoton");
+    boton.addEventListener("click", function(event) {
+      event.stopPropagation(); // Detiene la propagación del evento
+      alert("Hola!");
+    });
+div.addEventListener('click',()=>{alert("Hola! Soy el div");
 })
+})
+
+
+
